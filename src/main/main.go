64023f698d806f213b9cdbe0027d0c5450e60a77
Ankit Kumar <kumar.ankit55@gmail.com>
2022-04-11 00:34:19 -0400

    add net/rpc and basic server code

---
+++ src/main/main.go
@@ -0,0 +1,34 @@
+package chord
+
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+
+	"../chord"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: ./main <port>")
+	}
+	port := os.Args[1]
+
+	// Create DHT and start chord here.
+	ch := chord.Node{}
+	ch.Create("127.0.0.1:" + port)
+
+	httpServer(port)
+}
+
+func httpServer(port string) {
+	// TODO: All the rpc struct need to be registered with rpc.Register(...)
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", ":"+port)
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
+}
