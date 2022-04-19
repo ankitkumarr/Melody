b66d170f788ce64111572de0298bf603074c936c
Ankit Kumar <kumar.ankit55@gmail.com>
2022-04-18 23:25:42 -0400

    Add E2E DHT test setup

---
+++ src/common/hash.go
@@ -0,0 +1,21 @@
+package hash
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/binary"
+	"math"
+)
+
+func KeyHash(key string) int {
+	h := sha1.New()
+	h.Write([]byte(key))
+
+	bs := h.Sum(nil)
+	buf := bytes.NewBuffer(bs[:])
+
+	var sum int
+	binary.Read(buf, binary.LittleEndian, &sum)
+
+	return sum % int(math.Pow(2, 31))
+}
---
+++ test/test_melody.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd ../src/main
+
+go build main.go
+./main
